a3dc1ae675fd43a61de03a556bbc6696264bd8f3
KevinJaramilloGiraldo <kevin.jaramillo@correounivalle.edu.co>
2024-09-30 17:46:45 -0500

    Landing

--- scriptShoppingCar.js
+++ scriptShoppingCar.js
@@ -9,3 +9,22 @@ window.addEventListener("scroll", function() {
         header.classList.remove("scroll")
     }
 })
+
+// Obtener las referencias al botón y al audio
+const button = document.getElementById('floatbutton1');
+const song = document.getElementById('song');
+
+// Función para alternar entre reproducir y pausar
+function PlayOPause() {
+    if (song.paused) {
+        song.play();
+        button.textContent = 'Pause Music';
+    } else {
+        song.pause();
+        button.textContent = 'Play Music';
+    }
+}
+
+// Asignar el evento de clic al botón de música
+button.addEventListener('click', PlayOPause);
+
